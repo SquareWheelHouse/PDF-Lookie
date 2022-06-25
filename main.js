3c5bdf606e130721fa8521b44b47f724e0f5be17
eg33 <erikg33@gmail.com>
2022-06-25 11:02:51 -0400

    Hello World

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width : 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
