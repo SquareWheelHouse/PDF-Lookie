9cbd85680858e37b78a7acf3d54d4013d1e8cc39
eg33 <erikg33@gmail.com>
2022-06-27 00:16:22 -0400

    Add protocol handling * Has excess, unused commands for buttons * use pdf-lookie to launch * handle squirrel startup double launch issue * add package for determining if electron is dev

--- main.js
+++ main.js
@@ -1,28 +1,87 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const { app, BrowserWindow, ipcMain, shell, dialog } = require("electron")
+const path = require("path")
 
+//to manage double opening of app during installation using Squirrel
 if (require('electron-squirrel-startup')) {
-    app.quit();
-    process.exit(0);
+    app.quit()
+    process.exit(0)
 }
 
-const createWindow = () => {
-    const win = new BrowserWindow({
+let mainWindow;
+
+//protocol (external) access
+
+if (isDev && process.platform === 'win32') {
+    // Set the path of electron.exe and your app.
+    // These two additional parameters are only available on windows.
+    // Setting this is required to get this working in dev mode.
+    app.setAsDefaultProtocolClient('pdf-lookie', process.execPath, [
+      resolve(process.argv[1])
+    ]);
+  } else if (process.defaultApp) {
+    if (process.argv.length >= 2) {
+      app.setAsDefaultProtocolClient('pdf-lookie', process.execPath, [path.resolve(process.argv[1])])
+    }
+} else {
+    app.setAsDefaultProtocolClient('pdf-lookie')
+}
+
+const gotTheLock = app.requestSingleInstanceLock()
+
+const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+}
+
+if (!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore()
+            mainWindow.focus()
+        }
+    })
+
+    // Create mainWindow, load the rest of the app, etc...
+    app.whenReady().then(() => {
+        createWindow();
+    })
+
+    app.on('open-url', (event, url) => {
+        dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
+        replaceText('launch-url', `You arrived from: ${url}`)
+    })
+}
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
         width : 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true
         }
     });
 
-    win.loadFile(path.join(__dirname, "index.html"));
+    mainWindow.loadFile("index.html")
 }
 
-app.whenReady().then(() => {
-    createWindow()
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+app.on('open-url', (event, url) => {
+    replaceText('launch-url', `You arrived from: ${url}`)
+})
+
+// Handle window controls via IPC
+ipcMain.on('shell:open', () => {
+  const pageDirectory = __dirname.replace('app.asar', 'app.asar.unpacked')
+  const pagePath = path.join('file://', pageDirectory, 'index.html')
+  shell.openExternal(pagePath)
 })
--- preload.js
+++ preload.js
@@ -1,10 +1,22 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+const { contextBridge, ipcRenderer } = require('electron')
+
+// Set up context bridge between the renderer process and the main process
+contextBridge.exposeInMainWorld(
+  'shell',
+  {
+    open: () => ipcRenderer.send('shell:open'),
+  }
+)
+
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency]) 
-    }
-  })
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency]) 
+  }
+})
